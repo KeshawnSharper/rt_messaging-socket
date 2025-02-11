837787593f52eda4eec8e75ab366b1a854599c91
Keshawn Sharper <Ksharper@studentmba.org>
2025-02-11 11:23:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+// server/index.js
+const express = require("express");
+const axios = require("axios")
+const http = require("http").createServer();
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
+});
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+// Enable CORS for all requests
+app.use(cors());
+
+// Define a route handler for the root path
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+  
+  
+    socket.on('message', (data) => {
+      
+      console.log('Received message:', data);
+
+      io.emit('message', data); // Broadcasting the message
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
+
+// Start the server
+http.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
